c7060e997a1c590f6f60ff41e69e1fa20b1ffd3f
kalyansrinivaskodi <kalyansrinivas.kodi@gmail.com>
2021-06-27 00:16:47 +0530

    Create returnallprime.js

---
+++ 21june/returnallprime.js
@@ -0,0 +1,37 @@
+// Return all the prime numbers in an array
+var a=[3,5,7,8,9,6,11,15,19];
+let isprime=function(x)
+{
+    if(x===2)
+    {
+        return 1;
+    }
+    else if(x>2)
+    {
+        for(let i=2;i<x/2;i++)
+        {
+            if(x%i===0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+let pa=(function(a)
+{
+    let parr=[];
+    for(let i=0;i<a.length;i++)
+    {
+        if(isprime(a[i]))
+        {
+            parr.push(a[i]);
+        }
+    }
+    return parr;
+})
+console.log(pa(a));
