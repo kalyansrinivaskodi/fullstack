eed77caf567bf04b439fd3acc47ee430afcdc31e
kalyansrinivaskodi <kalyansrinivas.kodi@gmail.com>
2021-06-26 23:58:57 +0530

    Update TitleCaps.js

--- 21june/TitleCaps.js
+++ 21june/TitleCaps.js
@@ -1,11 +1,12 @@
 var str="Hi guys iam kalyan srinivas from IIT Madras mtech cse";
-function Titlecaps(string)
+Titlecaps=function (string)
 {
+    //anonymous function
     var sentences=string.toLowerCase().split(" ");
     for(let i=0;i<sentences.length;i++)
     {
         sentences[i]=sentences[i][0].toUpperCase()+sentences[i].slice(1);
     }
     return sentences;
-}
+};
 console.log(Titlecaps(str).join(" "));
