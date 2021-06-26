affa21b1efa71853a388e2aadf8fea8b15845f83
kalyansrinivaskodi <kalyansrinivas.kodi@gmail.com>
2021-06-27 02:38:13 +0530

    Create printoddnumbers.js

---
+++ 21june/arrowfunctions/printoddnumbers.js
@@ -0,0 +1,12 @@
+//print odd numbers using =>
+let b=[1,2,3,4,3,4,5,6,3,7]
+hel=(a)=>{for(let i=0;i<a.length;i++ )
+    {
+    if(a[i]%2===1)
+    {
+        console.log(a[i]);
+    }
+    }
+    
+}
+hel(b);
